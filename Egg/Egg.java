c1dcf0d0741fb9aeba469376b1377da881fc6eb9
mubasheerg <87425459+mubasheerg@users.noreply.github.com>
2021-07-20 17:39:54 +0530

    Create Egg.java

---
+++ Egg/Egg.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Egg {
+	public static void main(String[] asd) {
+		Scanner sc = new java.util.Scanner(System.in);
+		System.out.println("Enter the number of eggs");
+		int n = sc.nextInt();
+		System.out.println("Your number of eggs is ");
+		int g = n / 144;
+		System.out.print(g + " gross, ");
+		int r = n - (g * 144);
+		int d = r / 12;
+		System.out.print(d + " dozen, ");
+		r = r - d * 12;
+		System.out.print(" and " + r + " remaining");
+	}
+}
